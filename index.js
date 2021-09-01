ece446e65e5c7689eebdfa4686ae74b67ed918bc
lycho33 <cho.lyds@gmail.com>
2021-09-01 17:07:10 -0400

    commit tree bca53b4847bf3d0d6a941ed1316f52243abbcf94

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your functions here
+// function map(src, callback) {
+//     let newArray = []
+//     for (let i = 0; i < src.length; i++) {
+//         newArray.push(callback(src[i]))
+//     }
+//     return newArray
+// }
+
+// function reduce(src, callback, startingPoint = 0){
+//     let total = startingPoint
+
+//     if (startingPoint) {
+//         for (let i = 0; i < src.length; i++) {
+//             total = callback(total, src[i])
+//         }
+//         return total
+//     } else {
+//         total = src[0]
+//         for (let i = 
+//             0; i < src.length; i++) {
+//             total = callback(total, src[i])
+//         }
+//         return total
+//     }
+// }
+
+function map(array, callback){
+    let r = []
+    for (let i = 0; i < array.length; i++){
+        r.push(callback(array[i]))
+    }
+    return r
+}
